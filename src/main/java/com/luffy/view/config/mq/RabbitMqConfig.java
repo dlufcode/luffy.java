80259452950a0903365ad02b48cd6acacfcd0a35
liangjun <liangjun@bilibil.com>
2020-06-02 10:33:20 +0800

    rabbitmq

--- src/main/java/com/luffy/view/config/mq/RabbitMqConfig.java
+++ src/main/java/com/luffy/view/config/mq/RabbitMqConfig.java
@@ -1,5 +1,10 @@
 package com.luffy.view.config.mq;
 
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.MessageConverter;;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +16,25 @@ public class RabbitMqConfig {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUri("amqp://guest:guest@127.0.0.1:5672");
+        return factory;
+    }
+
+    @Bean(name = "myFactory")
+    public SimpleRabbitListenerContainerFactory myFactory() {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setMessageConverter(messageConverter());
+        factory.setConcurrentConsumers(1);
+        factory.setMaxConcurrentConsumers(1);
+        factory.setPrefetchCount(1);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setConnectionFactory(connectionFactory());
+        return factory;
     }
 }
--- src/main/java/com/luffy/view/service/rabbitMq/DelayQueueConsumer.java
+++ src/main/java/com/luffy/view/service/rabbitMq/DelayQueueConsumer.java
@@ -1,6 +1,8 @@
 package com.luffy.view.service.rabbitMq;
 
 import com.luffy.view.config.mq.RabbitDelayQueueConfig;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,25 @@ import java.util.Date;
 @Component
 public class DelayQueueConsumer {
 
-    @RabbitListener(queues = RabbitDelayQueueConfig.DELAY_QUEUE_NAME)
+
+    /**
+     * containerFactory rabbitListenerContainerFactory,myFactory
+     *
+     * @param message
+     * @param channel
+     * @param messageInfo
+     */
+    @RabbitListener(queues = RabbitDelayQueueConfig.DELAY_QUEUE_NAME, containerFactory = "myFactory")
     @RabbitHandler
-    public void consumer(String message) {
-        System.out.println(String.format("%s:%s", new Date().toString(), message));
+    public void consumer(String message, Channel channel, Message messageInfo) {
+        try {
+            // false 移除队列  true 不移除
+            channel.basicReject(messageInfo.getMessageProperties().getDeliveryTag(), false);
+            System.out.println(String.format("%s:%s", new Date().toString(), message));
+        } catch (Exception e) {
+            System.out.println("exception:" + e.getMessage());
+        }
+
     }
 
 }
